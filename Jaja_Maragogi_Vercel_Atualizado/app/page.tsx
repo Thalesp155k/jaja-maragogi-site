053ceb75455c457ff6a19966da8ac040da0e5531
Thales <thalessmith82@gmail.com>
2026-08-13 21:24:23 -0300

    Troca foto principal por png

--- Jaja_Maragogi_Vercel_Atualizado/app/page.tsx
+++ Jaja_Maragogi_Vercel_Atualizado/app/page.tsx
@@ -308,7 +308,7 @@ export default function Home() {
 
         <div className="hero-media">
   <img
-    src="/media/foto-principal-hero.jpg"
+    src="/media/foto-principal-hero.png"
     alt="Buggy vermelho em Maragogi"
   />
           <div className="hero-media-shade" />
